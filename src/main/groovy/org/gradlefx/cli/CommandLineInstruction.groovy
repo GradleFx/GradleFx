299227432a96029e6aac191c4cdb25491f75455a
RIAstar <maxime@riastar.net>
2012-11-18 22:04:02 +0100

    fixed #57: Proper error message for issue with FlexUnitApplication.mxml

--- src/main/groovy/org/gradlefx/cli/CommandLineInstruction.groovy
+++ src/main/groovy/org/gradlefx/cli/CommandLineInstruction.groovy
@@ -27,12 +27,12 @@ import org.slf4j.LoggerFactory
 
 
 abstract class CommandLineInstruction {
-    
+
     protected static final Logger LOG = LoggerFactory.getLogger 'gradlefx'
-    
+
     Project project
     GradleFxConvention flexConvention
-    
+
     List arguments
 
     public CommandLineInstruction(Project project) {
@@ -40,9 +40,9 @@ abstract class CommandLineInstruction {
         flexConvention = project.convention.plugins.flex
         arguments = []
     }
-    
+
     abstract public void setConventionArguments()
-    
+
     /** Adds the Flash player library to the <code>-external-library-path</code> argument */
     public void addPlayerLibrary() {
         String libPath = "$flexConvention.flexHome/frameworks/libs/player/{targetPlayerMajorVersion}.{targetPlayerMinorVersion}/playerglobal.swc"
@@ -52,7 +52,7 @@ abstract class CommandLineInstruction {
     /** Adds the framework libraries to the arguments based on the {@link FrameworkLinkage} */
     public void addFramework() {
         FrameworkLinkage linkage = flexConvention.frameworkLinkage
-        
+
         //ii's a pure AS project: we don't want to load the Flex configuration
         if (!linkage.usesFlex())
             reset CompilerOption.LOAD_CONFIG
@@ -60,32 +60,32 @@ abstract class CommandLineInstruction {
         else if (!linkage.isCompilerDefault(flexConvention.type) || (flexConvention.type == FlexType.swc)) {
             //remove RSL's defined in config.xml
             reset CompilerOption.RUNTIME_SHARED_LIBRARY_PATH
-            
+
             //set the RSL's defined in config.xml on the library path
             def flexConfig = new XmlSlurper().parse(flexConvention.configPath)
             def relativeSwcPaths = flexConfig['runtime-shared-library-path']['path-element']
-            
+
             Collection paths = relativeSwcPaths.collect { "$flexConvention.flexHome/frameworks/$it" }
             addAll linkage.getCompilerOption(), paths
         }
     }
-    
+
     /** Adds the <code>-source-path</code> argument based on project.srcDirs and project.localeDir */
     public void addSourcePaths() {
         addAll CompilerOption.SOURCE_PATH, getValidSourcePaths()
-        
+
         //add locale path to source paths if any locales are defined
         if (flexConvention.locales && flexConvention.locales.size()) {
             add CompilerOption.SOURCE_PATH, project.file(flexConvention.localeDir).path + '/{locale}'
         }
     }
-    
-    protected Collection <List> getValidSourcePaths() {
+
+    protected Collection<List> getValidSourcePaths() {
         //don't allow non existing source paths unless they contain a token (e.g. {locale})
         //TODO {} tokens can be validated earlier: locale paths should be in localeDir property
         return flexConvention.srcDirs
-            .findAll { String path -> project.file(path).exists() || path.contains('{') }
-            .collect { project.file(it).path }
+                .findAll { String path -> project.file(path).exists() || path.contains('{') }
+                .collect { project.file(it).path }
     }
 
     /** Adds the <code>-locale</code> argument based on {@link GradleFxConvention}.locales */
@@ -94,22 +94,22 @@ abstract class CommandLineInstruction {
             set CompilerOption.LOCALE, flexConvention.locales
         }
     }
-    
+
     public void addInternalLibraries() {
         Configuration internal = project.configurations.internal
         addLibraries internal.files, internal, CompilerOption.INCLUDE_LIBRARIES
     }
-    
+
     public void addExternalLibraries() {
         Configuration external = project.configurations.external
         addLibraries external.files, external, CompilerOption.EXTERNAL_LIBRARY_PATH
     }
-    
+
     public void addMergedLibraries() {
         Configuration merged = project.configurations.merged
         addLibraries merged.files, merged, CompilerOption.LIBRARY_PATH
     }
-    
+
     public void addTheme() {
         Configuration theme = project.configurations.theme
         addLibraries theme.files, theme, CompilerOption.THEME
@@ -117,27 +117,27 @@ abstract class CommandLineInstruction {
 
     /**
      * Adds all the dependencies for the given configuration as compile arguments
-     * 
+     *
      * @param libraryFiles
      * @param configuration
      * @param compilerOption
      */
-    protected void addLibraries(Set <File> libraryFiles, Configuration configuration, CompilerOption compilerOption) {
+    protected void addLibraries(Set<File> libraryFiles, Configuration configuration, CompilerOption compilerOption) {
         //only add swc dependencies, no use in adding pom dependencies
-        Collection <File> files = libraryFiles.findAll { 
+        Collection<File> files = libraryFiles.findAll {
             it.name.endsWith(FlexType.swc.toString()) || it.isDirectory()
         }
         validateFilesExist files, configuration
-        
+
         Collection paths = files.collect { it.path }
         addAll compilerOption, paths
     }
-    
+
     /** Adds the <code>-runtime-shared-library-path</code> argument based on RSL dependencies */
     public void addRSLs() {
         Configuration rsl = project.configurations.rsl
         validateFilesExist rsl.files, rsl
-        
+
         Collection paths = rsl.files.collect { "$it.path,${it.name[0..-2]}f" }
         addAll CompilerOption.RUNTIME_SHARED_LIBRARY_PATH, paths
     }
@@ -147,7 +147,7 @@ abstract class CommandLineInstruction {
             def flexConfig = new XmlSlurper().parse(flexConvention.configPath)
             flexConfig['runtime-shared-library-path'].each {
                 String swcName = "${flexConvention.flexHome}/frameworks/" + it['path-element'].text()
-                String libName =  it['rsl-url'][1].text()[0..-2] + 'f'
+                String libName = it['rsl-url'][1].text()[0..-2] + 'f'
 
                 File dependency = new File(swcName)
                 if (!dependency.exists()) {
@@ -159,41 +159,41 @@ abstract class CommandLineInstruction {
         }
     }
 
-    
-    private void validateFilesExist(Collection <File> files, Configuration configuration) {
-        Collection <File> ghosts = files.findAll { !it.exists() }
-        
+
+    private void validateFilesExist(Collection<File> files, Configuration configuration) {
+        Collection<File> ghosts = files.findAll { !it.exists() }
+
         if (ghosts && ghosts.size()) {
             throw new ResolveException(configuration, new Throwable(
-                "Couldn't find some dependency files - are you sure the path is correct? " +
-                "Unresolved dependency paths: ${ghosts.collect { it.path }}"
+                    "Couldn't find some dependency files - are you sure the path is correct? " +
+                            "Unresolved dependency paths: ${ghosts.collect { it.path }}"
             ))
         }
     }
-    
+
     public void addAdditionalCompilerOptions() {
         addAll flexConvention.additionalCompilerOptions
     }
-    
+
     protected void addOutput(String dir) {
         set CompilerOption.OUTPUT, project.file(dir).path
     }
-    
+
     public void execute(AntBuilder ant, String antTask) {
         LOG.info "Compiling with $antTask"
         arguments.each {
             LOG.info "\t$it"
         }
-        
+
         String antResultProperty = antTask + 'Result'
         String antOutputProperty = antTask + 'Output'
-        
-        ant.java (
-            jar:            "$flexConvention.flexHome/lib/${antTask}.jar",
-            dir:            "$flexConvention.flexHome/frameworks",
-            fork:           true,
-            resultproperty: antResultProperty,
-            outputproperty: antOutputProperty
+
+        ant.java(
+                jar: "$flexConvention.flexHome/lib/${antTask}.jar",
+                dir: "$flexConvention.flexHome/frameworks",
+                fork: true,
+                resultproperty: antResultProperty,
+                outputproperty: antOutputProperty
         ) {
             flexConvention.jvmArguments.each {
                 jvmarg(value: it)
@@ -203,46 +203,66 @@ abstract class CommandLineInstruction {
                 arg(value: it)
             }
         }
-        
+
         String output = ant.properties[antOutputProperty]
         if (ant.properties[antResultProperty] == '0') LOG.info "[$antTask] $output"
-        else throw new Exception("$antTask execution failed: $output\n")
+        else handleError antTask, output
     }
-    
+
+    protected void handleError(String antTask, String message) {
+        if (message.contains("FlexUnitTestRunnerUI")) {
+            message += '\n[GradleFx tip] Chances are you\'re using the FlashBuilder IDE.' +
+                    '\nWhen you run unit tests from within FlashBuilder ' +
+                    '(by selecting "Execute FlexUnit Tests" from the project\'s context menu), ' +
+                    'it will automatically create a test application for you called FlexUnitApplication.mxml ' +
+                    'and there\'s nothing you can change about that (short of not using the feature). ' +
+                    'What\'s more, this application has lots of dependencies you haven\'t defined ' +
+                    'in your build script, hence the compiler error.' +
+                    '\nThe easiest way to fix this, is to add the following code to your build script:' +
+                    '\n\n\tcompileFlex.doFirst {' +
+                    '\n\t\tproject.file(srcDirs[0] + \'/FlexUnitApplication.mxml\').delete()' +
+                    '\n\t}' +
+                    '\n\nThis will delete the file prior to compiling and FlashBuilder will not complain, ' +
+                    'since it will automatically regenerate it when needed.';
+        }
+
+        throw new Exception("$antTask execution failed: $message\n")
+    }
+
     public void set(CompilerOption option, String value) {
         arguments.add "$option.optionName=$value"
     }
-    
+
     public void set(CompilerOption option, Collection<String> values) {
         arguments.add "$option.optionName=${values.join(',')}"
     }
-    
+
     public void reset(CompilerOption option) {
         arguments.add "$option.optionName="
     }
-    
+
     public void add(CompilerOption option, String value) {
         arguments.add "$option.optionName+=$value"
     }
-    
+
     public void add(CompilerOption option) {
         arguments.add option.optionName
     }
-    
+
     public void add(String value) {
         arguments.add value
     }
-    
+
     public void setAll(CompilerOption option, Collection<String> values) {
         values.each { set option, it }
     }
-    
+
     public void addAll(CompilerOption option, Collection<String> values) {
         values.each { add option, it }
     }
-    
+
     public void addAll(Collection<String> values) {
         arguments.addAll values
     }
-    
+
 }
