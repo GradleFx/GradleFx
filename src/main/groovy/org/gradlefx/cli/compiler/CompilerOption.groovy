d32443c65de1ba0648ce31c0f0cadd887b08b94a
Matyas Forian Szabo <idontgivemy@email.com>
2015-04-12 01:46:41 +0100

    Add the -arch package option for AIR Android. This allows to use the -arch x86 option for x86 Android devices.

--- src/main/groovy/org/gradlefx/cli/compiler/CompilerOption.groovy
+++ src/main/groovy/org/gradlefx/cli/compiler/CompilerOption.groovy
@@ -1363,8 +1363,15 @@ public enum CompilerOption {
      * version: AIR 14 and higher
      *
      */
-    USE_LEGACY_COMPILER("-useLegacyAOT")
+    USE_LEGACY_COMPILER("-useLegacyAOT"),
 
+    /**
+     * (Android only, AIR 14 and higher) Application developers can use this argument to create APK for x86 platforms,
+     * it takes following values:
+     * armv7 - ADT packages APK for the Android armv7 platform. This is the default value when no value is specified.
+     * x86 - ADT packages APK for the Android x86 platform.
+     */
+    ARCH("-arch")
 
     private String optionName;
 
